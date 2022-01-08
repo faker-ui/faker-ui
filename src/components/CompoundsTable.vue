fd1553d44e6e641a65df453aa83460b307dd09e5
Alex Scott <codetheorist@outlook.com>
2022-01-08 20:10:23 +0000

    Add compounds table component

---
+++ src/components/CompoundsTable.vue
@@ -0,0 +1,33 @@
+<template>
+  <b-table :fields="fields" :items="items" show-empty />
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  },
+  data() {
+    return {
+      fields: [
+        {
+          key: "label",
+          label: "Label"
+        },
+        {
+          key: "example",
+          label: "Example",
+          formatter: (value,key,item) => item.value
+        },
+        {
+          key: "actions",
+          label: ""
+        }
+      ]
+    }
+  }
+}
+</script>
